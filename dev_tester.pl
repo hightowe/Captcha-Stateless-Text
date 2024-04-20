5f5aa7b0a57d6a8b95bd61180c1ebb9ef39e82e0
Lester Hightower <lester.hightower@gmail.com>
2024-04-20 17:59:46 -0400

    Added protections for bad enc_payload to validate()

--- dev_tester.pl
+++ dev_tester.pl
@@ -9,6 +9,7 @@ use Captcha::Stateless::Text;
 my $captcha = Captcha::Stateless::Text->new();
 $captcha->set_iv('gkbx5g9hsvhqrosg');                  # Must be 16 bytes / 128 bits
 $captcha->set_key('tyDjb39dQ20pdva0lTpyuiowWfxSSwa9'); # 32 bytes / 256 bits (AES256)
+my $ep_pre = $captcha->get_ep_pre();
 
 my $qa={};
 foreach my $type (qw(chars math)) {
@@ -30,23 +31,23 @@ sub get_QA_array($$) {
 my $tests = {
   'chars_invalid' => {
     'a' => 'GVS',
-      'enc_payload' => 'qctvYSVBfjzYh1V0DfrZ23Yd_-nmOHbJdC7JUlUKMfZ6I13qt2R5vSePdWS0vAJS1Z3WgeK-q1s5U3BOv2F8XqYmnoCjfHX8f5Q83AjexhO_FRAi_3rl40PPdx2RR6rf',
+      'enc_payload' => $ep_pre.'qctvYSVBfjzYh1V0DfrZ23Yd_-nmOHbJdC7JUlUKMfZ6I13qt2R5vSePdWS0vAJS1Z3WgeK-q1s5U3BOv2F8XqYmnoCjfHX8f5Q83AjexhO_FRAi_3rl40PPdx2RR6rf',
       'q' => 'Provide the second, third, and sixth characters from B-G-Q-E-O-S',
       'a_real' => 'GQS',
     },
   'math_invalid' => {
     'a' => 6,
-      'enc_payload' => 'HjSg-HEJJMMIj55rDfyLOehzmhzpepwvZU8kOePW5Sk',
+      'enc_payload' => $ep_pre.'HjSg-HEJJMMIj55rDfyLOehzmhzpepwvZU8kOePW5Sk',
       'q' => '7 + 3',
       'a_real' => '10',
     },
   'chars' => {
     'a' => 'GQS',
-      'enc_payload' => 'qctvYSVBfjzYh1V0DfrZ23Yd_-nmOHbJdC7JUlUKMfZ6I13qt2R5vSePdWS0vAJS1Z3WgeK-q1s5U3BOv2F8XqYmnoCjfHX8f5Q83AjexhO_FRAi_3rl40PPdx2RR6rf',
+      'enc_payload' => $ep_pre.'qctvYSVBfjzYh1V0DfrZ23Yd_-nmOHbJdC7JUlUKMfZ6I13qt2R5vSePdWS0vAJS1Z3WgeK-q1s5U3BOv2F8XqYmnoCjfHX8f5Q83AjexhO_FRAi_3rl40PPdx2RR6rf',
     },
   'math' => {
     'a' => 10,
-      'enc_payload' => 'HjSg-HEJJMMIj55rDfyLOehzmhzpepwvZU8kOePW5Sk',
+      'enc_payload' => $ep_pre.'HjSg-HEJJMMIj55rDfyLOehzmhzpepwvZU8kOePW5Sk',
     },
   };
 
